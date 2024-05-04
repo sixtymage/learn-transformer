e2890b5810f4455f8ced3b79f39fb69830e76523
Jon Hutchings <sixty.mage@outlook.com>
2024-05-04 11:51:51 +0200

    Add initial files

---
+++ src/LearnTransformer/LearnTransformer/DataLoader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnTransformer
+{
+  internal class DataLoader(string path)
+  {
+    public void Load()
+    {
+      File.ReadAllText(path);
+    }
+  }
+}
